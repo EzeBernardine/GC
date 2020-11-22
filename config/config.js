7a4ca627a47bc8649019d9ad883f462eeb20bb3d
EzeBernardine <demayous2@gmail.com>
2020-11-22 09:21:01 +0100

    Changes

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 // Your personal access token
 // const token = "9e698745e3a0606393c8e66d5be795a63680185e"; //eazi
 // const token = "74dcfe414a3431133709db513c75dfd03116dd18"; //me
-const token = "b71d7e2f644cf032db46f7e229378fe2845926d2"; //new me
+const token = "f2d679455ecfa89f76a231aa724d4e81a3be12e0"; //new me
 module.exports = {
     token
 }
