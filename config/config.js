96a31b5213ee40061a29f9048cd310e57b767fc7
EzeBernardine <demayous2@gmail.com>
2020-11-22 09:27:02 +0100

    Added

--- config/config.js
+++ config/config.js
@@ -1,7 +1,6 @@
 // Your personal access token
-// const token = "9e698745e3a0606393c8e66d5be795a63680185e"; //eazi
-// const token = "74dcfe414a3431133709db513c75dfd03116dd18"; //me
-const token = "9d47381832ba8ffe44290ed1111e2e9a2e128e93"; //new me
+
+const token = "9d47381832ba8ffe44290ed1111e2e9a2e128e93";
 module.exports = {
-    token
-}
+  token,
+};
