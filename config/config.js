3fee792f4f583a10b8e9b29e3ac3daed59ef72c5
EzeBernardine <demayous2@gmail.com>
2020-11-22 09:25:02 +0100

    Fixed

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 // Your personal access token
 // const token = "9e698745e3a0606393c8e66d5be795a63680185e"; //eazi
 // const token = "74dcfe414a3431133709db513c75dfd03116dd18"; //me
-const token = "10ee49aadb8411d60c26d5e52c8e3b5bc2651851"; //new me
+const token = "9d47381832ba8ffe44290ed1111e2e9a2e128e93"; //new me
 module.exports = {
     token
 }
