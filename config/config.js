cc748ae8cc177d4bbfcacaaa859842f0945f5360
EzeBernardine <demayous2@gmail.com>
2020-11-22 09:17:44 +0100

    Removed config from gitignore

---
+++ config/config.js
@@ -0,0 +1,7 @@
+// Your personal access token
+// const token = "9e698745e3a0606393c8e66d5be795a63680185e"; //eazi
+// const token = "74dcfe414a3431133709db513c75dfd03116dd18"; //me
+const token = "b71d7e2f644cf032db46f7e229378fe2845926d2"; //new me
+module.exports = {
+    token
+}
